e93cd4dc3805f54bbf961396dd9144f510728452
mekhti11 <mekhti11@gmail.com>
2018-05-30 17:14:31 +0300

    added contact entity

---
+++ app/src/main/java/com/mekhti/smartsmsbox/Entity/Contact.java
@@ -0,0 +1,30 @@
+package com.mekhti.smartsmsbox.Entity;
+
+public class Contact {
+
+    private String name;
+    private String phoneNum;
+
+    public Contact(String name, String phoneNum) {
+        this.name = name;
+        this.phoneNum = phoneNum;
+    }
+
+    public Contact(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+}
